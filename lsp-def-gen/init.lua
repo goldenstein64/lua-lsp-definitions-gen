423b51140c56c6ed4c40ca806d48342df0e73c3b
goldenstein64 <goldnstein64@gmail.com>
2023-12-12 15:46:46 -0500

    feat!: only client -> server routes are created

--- lsp-def-gen/init.lua
+++ lsp-def-gen/init.lua
@@ -9,50 +9,18 @@ local REQUEST_PATH_FORMAT = "out/routes/%s.lua"
 local REQUEST_DIR_FORMAT = "out/routes/%s"
 
 local REQUEST_READ_FORMAT = [[
-local register = require("lsp.register")
-
 ---@param params lsp.Request.%s.params
 ---@return lsp.Response.%s.result
-register["%s"] = function(params) end
-]]
-
-local REQUEST_WRITE_FORMAT = [[
-local request = require("lsp.request")
-
----@return boolean, lsp.Response.%s.result | lsp.Response.%s.error
-function request.%s(params)
-	return request("%s", params)
-end
+return function(params) end
 ]]
 
 local NOTIFICATION_PATH_FORMAT = "out/routes/%s.lua"
 local NOTIFICATION_DIR_FORMAT = "out/routes/%s"
 
 local NOTIFICATION_READ_FORMAT = [[
-local register = require("lsp.register")
-
 ---@param params lsp.Notification.%s.params
 ---@return nil -- notifications don't expect a response
-register["%s"] = function(params) end
-]]
-
-local NOTIFICATION_WRITE_FORMAT = [[
-local notify = require("lsp.notify")
-
----@return lsp.Notification.%s.params
-function notify.%s() end
-]]
-
-local NOTIFICATION_READ_WRITE_FORMAT = [[
-local register = require("lsp.register")
-local notify = require("lsp.notify")
-
----@return lsp.Notification.%s.params
-function notify.%s() end
-
----@param params lsp.Notification.%s.params
----@return nil -- notifications don't expect a response
-register["%s"] = function(params) end
+return function(params) end
 ]]
 
 ---@param path string
@@ -104,30 +72,29 @@ return function()
 			for _, request in ipairs(object.requests) do
 				local method = request.method
 				local methodTypeName = method:gsub("%$", "_"):gsub("/", "-")
-				local methodName = method:gsub("%$/", ""):gsub("/", "_")
-				local moduleName = method:match("/([^/]+)$")
-				local parentPath
-				if moduleName then
-					parentPath = assert(method:match("^(.+)/[^/]+$"), "parent path not found")
-					ensureDir(REQUEST_DIR_FORMAT:format(parentPath))
-				else
-					moduleName = method
-				end
-
 				local dir = request.messageDirection
 				local content
 				if dir == "clientToServer" then
 					content = REQUEST_READ_FORMAT:format(methodTypeName, methodTypeName, method)
-				elseif dir == "serverToClient" then
-					content = REQUEST_WRITE_FORMAT:format(methodTypeName, methodTypeName, methodName, method)
-				else
+				elseif dir ~= "serverToClient" then
 					error(string.format("unhandled message direction '%s'", dir))
 				end
 
-				local requestPath = REQUEST_PATH_FORMAT:format(method)
-				local requestFile = assert(io.open(requestPath, "w"))
-				requestFile:write(content)
-				requestFile:close()
+				if content then
+					local moduleName = method:match("/([^/]+)$")
+					local parentPath
+					if moduleName then
+						parentPath = assert(method:match("^(.+)/[^/]+$"), "parent path not found")
+						ensureDir(REQUEST_DIR_FORMAT:format(parentPath))
+					else
+						moduleName = method
+					end
+
+					local requestPath = REQUEST_PATH_FORMAT:format(method)
+					local requestFile = assert(io.open(requestPath, "w"))
+					requestFile:write(content)
+					requestFile:close()
+				end
 			end
 		end
 
@@ -136,29 +103,25 @@ return function()
 				-- generate a file in routes
 				local method = notification.method
 				local methodTypeName = method:gsub("%$", "_"):gsub("/", "-")
-				local methodName = method:gsub("%$/", ""):gsub("/", "_")
-				local moduleName = method:match("/([^/]+)$")
-				if moduleName then
-					local parentPath = assert(method:match("^(.+)/[^/]+$"), "parent path not found")
-					ensureDir(NOTIFICATION_DIR_FORMAT:format(parentPath))
-				else
-					moduleName = method
-				end
-
-
 				local dir = notification.messageDirection
 				local content
-				if dir == "clientToServer" then
+				if dir == "clientToServer" or dir =="both" then
 					content = NOTIFICATION_READ_FORMAT:format(methodTypeName, method)
-				elseif dir == "serverToClient" then
-					content = NOTIFICATION_WRITE_FORMAT:format(methodTypeName, methodName)
-				else
-					content = NOTIFICATION_READ_WRITE_FORMAT:format(methodTypeName, methodName, methodTypeName, method)
 				end
 
-				local notificationFile = assert(io.open(NOTIFICATION_PATH_FORMAT:format(method), "w"))
-				notificationFile:write(content)
-				notificationFile:close()
+				if content then
+					local moduleName = method:match("/([^/]+)$")
+					if moduleName then
+						local parentPath = assert(method:match("^(.+)/[^/]+$"), "parent path not found")
+						ensureDir(NOTIFICATION_DIR_FORMAT:format(parentPath))
+					else
+						moduleName = method
+					end
+
+					local notificationFile = assert(io.open(NOTIFICATION_PATH_FORMAT:format(method), "w"))
+					notificationFile:write(content)
+					notificationFile:close()
+				end
 			end
 		end
 	end
